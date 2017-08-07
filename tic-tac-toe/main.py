7d08c5f18383603f9b46237f7ee568de1a976251
Samuel Peters <hello57748@Christophers-MacBook-Air.local>
2017-08-07 16:16:08 -0700

    start to html file

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -16,9 +16,11 @@
 #
 import webapp2
 
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('hello world!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
